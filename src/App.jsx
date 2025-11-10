e2206043322ad4536c16320b2681e46f22720472
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-11-10 08:11:15 -0600

    refactor: copy instructors solution to React useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,35 @@
-import { useLocalStorage } from "@uidotdev/usehooks"
-
-import "./App.css";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 export default function App() {
   const [todos, setTodos] = useLocalStorage("todos", []); // Tracks todo list
-  const [input, setInput] = useLocalStorage("todoInput", ""); // Tracks textbox input value
-
-
-  const addTodo = (text) => {
-    const trimmed = (text || "").trim(); // Trim removes any whitespace. This will prevent adding empty todos
-    if (!trimmed) return; // Return early if trimmed text is empty
-    setTodos([...todos, { id: Date.now(), text: trimmed }]) // Date.now generates a unique id based on the current timestamp
-  }
 
   return (
-    <>
-      <div>
-      <h1>Todos</h1>
+    <main className="flex flex-col gap-4 bg-slate-900 text-white p-4 min-h-screen justify-center items-center">
+      <h1 className="text-2xl uppercase tracking-wider font-black">Todos</h1>
       <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") { // Check that Enter key is pressed
-            addTodo(input); // Adds input value to list when Enter key is pressed
-            setInput(""); // Clears textbox
-          }
+        className="border p-2 rounded"
+        type="text"
+        id="todo-input"
+        aria-label="Add Todo"
+        onBlur={(e) => {
+          setTodos([...todos, { id: Date.now(), text: e.target.value }]); // Adds input value to list on blur
+          e.target.value = ""; // Clears textbox
         }}
-        placeholder="Add a todo..."
       />
-      <button onClick={() => {
-            addTodo(input); // Adds input value to list when button is clicked
-            setInput(""); // Clears textbox
-          }}>
-        Add Todo
-      </button>
-      <ul>
-        {todos.map((todo) => ( // Iterate over the todos array
-          <li key={todo.id}>{todo.text}</li> // Display each todo as a list item
+      <button className="bg-green-500 rounded px-2 py-4">Add Todo</button>
+      <ul className="text-center">
+        {todos.map(({ id, text }) => (
+          <li
+            key={id}
+            className="border-b border-slate-600 py-2 last:border-b-0"
+          >
+            {text}
+          </li> // Display each todo as a list item
         ))}
       </ul>
-      <button onClick={() => setTodos([])}>Clear Todos</button> {/* Button to clear all todos */}
-    </div>
-    </>
+      <button onClick={() => setTodos([])}>Clear Todos</button>
+      {""}
+      {/* Button to clear all todos */}
+    </main>
   );
 }
